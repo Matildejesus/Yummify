53742103f8134b6661c5e6c6b671ebc2deb9488e
Matildejesus <malvesdejesu@student.unimelb.edu.au>
2023-10-05 01:01:23 +1100

    find-recipe runs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import "dotenv/config";
-import cookie from "cookie-parser";
+//import cookieParser from "cookie-parser";
 
 const app = express();
 const apiKey = process.env.API_KEY;
@@ -11,117 +11,131 @@ const apiUrl = "https://api.spoonacular.com/recipes";
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookie());
+//app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { isFindRecipeRoute: false });
 });
 
 app.get("/faq", (req, res) => {
   res.render("faq.ejs");
 });
 
-function getTime() {
-  const timeOfDay = new Date();
-  timeOfDay.setHours(23, 59, 59, 999);
-  return timeOfDay;
-}
+async function getRecipe(inputs) {
+  const { selectedDiet, selectedIntolerance, numberOfRecipes, ingredients } =
+    inputs;
+  let query = `${apiUrl}/random?apiKey=${apiKey}&number=${numberOfRecipes}`;
+  let tags = [];
 
-function getMidnightTime() {
-  const midnight = new Date();
-  midnight.setHours(0, 0, 0, 0);
-  return midnight;
-}
-async function getRecipe(selectedDiet, selectedIntolerance, numberOfRecipes) {
+  if (selectedDiet !== "omnivore" && selectedDiet !== "select a diet") {
+    tags.push(selectedDiet);
+  }
+
+  if (selectedIntolerance !== "select an intolerance") {
+    tags.push(selectedIntolerance);
+  }
+
+  if (ingredients) {
+    tags.push(ingredients);
+  }
+  if (tags) {
+    query += `&tags=${tags.join(",")}`;
+  }
   try {
-    let result;
-    if (
-      (selectedDiet == "omnivore" || selectedDiet == "select a diet") &&
-      selectedIntolerance == "select an intolerance"
-    ) {
-      result = await axios.get(
-        `${apiUrl}/random?apiKey=${apiKey}&number=${numberOfRecipes}`
-      );
-    } else if (selectedDiet == "omnivore" || selectedDiet == "select a diet") {
-      result = await axios.get(
-        `${apiUrl}/random?apiKey=${apiKey}&number=${numberOfRecipes}&tags=${selectedIntolerance}`
-      );
-    } else if (selectedIntolerance == "select an intolerance") {
-      result = await axios.get(
-        `${apiUrl}/random?apiKey=${apiKey}&number=${numberOfRecipes}&tags=${selectedDiet}`
-      );
-    } else {
-      result = await axios.get(
-        `${apiUrl}/random?apiKey=${apiKey}&number=${numberOfRecipes}&tags=${selectedDiet},${selectedIntolerance}`
-      );
-    }
-    return result.data;
+    const result = await axios.get(query);
+    return result.data.recipes;
   } catch (error) {
     throw error;
   }
 }
 
-app.post("/find-recipe", async (req, res) => {
-  let selectedDiet = req.body.selectedDiet.toLowerCase();
-  let selectedIntolerance = req.body.selectedIntolerance.toLowerCase();
-  console.log(selectedDiet, selectedIntolerance);
-  const numberOfRecipes = 3;
-  try {
-    const result = await getRecipe(
-      selectedDiet,
-      selectedIntolerance,
-      numberOfRecipes
-    );
-
-    const recipes = result.recipes;
-    res.render("find-recipe.ejs", { content: recipes });
-    // res.json(result.data);
-  } catch (error) {
-    res.render("find-recipe.ejs", { content: error.response.data });
-    res.redirect("index.ejs");
-  }
-});
-
 app.post("/recipe", async (req, res) => {
-  let selectedDiet = req.body.selectedDiet.toLowerCase();
-  let selectedIntolerance = req.body.selectedIntolerance.toLowerCase();
-  const timeOfDay = getTime();
-  const endOfDay = getMidnightTime();
-  const numberOfRecipes = 7;
-  const filteredRecipes = [];
-
-  console.log(selectedDiet, selectedIntolerance);
-  console.log(endOfDay.getTime());
-
+  //console.log("Existing Cookie:", req.cookies.recipeOfTheDay);
   try {
-    const result = await getRecipe(
+    let recipes = []; // Declare the variable at the function scope.
+    const filteredRecipes = [];
+    // Check if there's data in the recipeOfTheDay cookie.
+    // if (
+    //   req.cookies.recipeOfTheDay &&
+    //   req.cookies.recipeOfTheDay !== "undefined"
+    // ) {
+    //   recipes = JSON.parse(req.cookies.recipeOfTheDay);
+    // } else {
+    let selectedDiet = req.body.selectedDiet.toLowerCase();
+    let selectedIntolerance = req.body.selectedIntolerance.toLowerCase();
+    const numberOfRecipes = 7;
+
+    const result = await getRecipe({
       selectedDiet,
       selectedIntolerance,
-      numberOfRecipes
-    );
+      numberOfRecipes,
+    });
 
-    const recipes = result.recipes;
+    recipes = result;
+    console.log(recipes.length);
     let count = 0;
-    recipes.forEach((recipe) => {
-      if (recipe.extendedIngredients.length <= 8) {
-        filteredRecipes.push(recipe);
+    recipes.forEach((recipeItem) => {
+      if (recipeItem.extendedIngredients.length <= 8) {
+        filteredRecipes.push(recipeItem);
         count++;
         if (count == 7 && filteredRecipes.length == 0) {
-          filteredRecipes.push(recipe);
+          filteredRecipes.push(recipeItem);
         }
       }
     });
-    // console.log(filteredRecipes[0]);
-    console.log(req.cookies.recipeOfTheDay);
-    res.cookie("recipeOfTheDay", filteredRecipes[0]);
-    console.log(req.cookies.recipeOfTheDay);
+
+    // console.log("filteredRecipe 1: " + JSON.stringify(filteredRecipes[0]));
+    // res.cookie("recipeOfTheDay", JSON.stringify(filteredRecipes));
+    // console.log("Cookie after setting:", req.cookies.recipeOfTheDay);
+    recipes = filteredRecipes;
+    // }
 
     res.render("recipe.ejs", {
-      content: JSON.parse(req.cookies.recipeOfTheDay),
+      content: recipes,
     });
-    // res.json(result.data);
   } catch (error) {
-    res.render("recipe.ejs", { content: error.response.data });
+    console.error("Error:", error);
+    res.render("recipe.ejs", {
+      content: error.response ? error.response.data : {},
+    });
+  }
+});
+
+app.post("/find-recipe", async (req, res) => {
+  let selectedDiet = req.body.selectedDiet.toLowerCase();
+  let selectedIntolerance = req.body.selectedIntolerance.toLowerCase();
+  res.render("find-recipe.ejs", {
+    selectedDiet: selectedDiet,
+    selectedIntolerance: selectedIntolerance,
+    isFindRecipeRoute: true,
+  });
+});
+
+app.post("/find-recipe-filtered", async (req, res) => {
+  // console.log("body: ", req.body);
+  let ingredients = req.body.ingredients;
+  let numberOfRecipes = req.body.count;
+  let selectedDiet = req.body.selectedDiet;
+  let selectedIntolerance = req.body.selectedIntolerance;
+  console.log(ingredients, numberOfRecipes, selectedDiet, selectedIntolerance);
+  try {
+    const result = await getRecipe({
+      selectedDiet,
+      selectedIntolerance,
+      numberOfRecipes,
+      ingredients,
+    });
+
+    const recipes = result;
+    console.log(recipes);
+    res.render("find-recipe.ejs", {
+      content: recipes,
+      isFindRecipeRoute: false,
+      selectedDiet: selectedDiet,
+      selectedIntolerance: selectedIntolerance,
+    });
+  } catch (error) {
+    res.redirect("index.ejs");
   }
 });
 
